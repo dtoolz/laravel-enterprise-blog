488e0429e1f6fd93793993bafbec18f49882aacf
dtoolz <donaldaudriel@gmail.com>
2023-08-13 15:14:46 +0100

    added the admin login view, admin middleware for route auth guard

--- routes/admin.php
+++ routes/admin.php
@@ -1,6 +1,14 @@
 <?php
+
+use App\Http\Controllers\Admin\AdminAuthenticationController;
+use App\Http\Controllers\Admin\DashboardController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('test', function(){
-    return 'working';
+
+Route::group(['prefix' => 'admin', 'as' => 'admin.'], function(){
+   Route::get('login', [AdminAuthenticationController::class, 'login'])->name('login');
+});
+
+Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => ['admin']], function(){
+   Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
 });

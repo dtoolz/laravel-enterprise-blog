ddcbabee0e87666df05d12a87b9ab2266ba4e4f6
dtoolz <donaldaudriel@gmail.com>
2023-09-04 07:30:14 +0100

    admin getting all news post on the news index page

--- resources/views/admin/news/index.blade.php
+++ resources/views/admin/news/index.blade.php
@@ -19,65 +19,99 @@
                 <ul class="nav nav-tabs" id="myTab2" role="tablist">
                     @foreach ($languages as $language)
                         <li class="nav-item">
-                            <a class="nav-link {{ $loop->index === 0 ? 'active' : '' }}" id="home-tab2" data-toggle="tab" href="#home-{{ $language->lang }}" role="tab"
-                                aria-controls="home" aria-selected="true">{{ $language->name }}</a>
+                            <a class="nav-link {{ $loop->index === 0 ? 'active' : '' }}" id="home-tab2" data-toggle="tab"
+                                href="#home-{{ $language->lang }}" role="tab" aria-controls="home"
+                                aria-selected="true">{{ $language->name }}</a>
                         </li>
                     @endforeach
                 </ul>
                 <div class="tab-content tab-bordered" id="myTab3Content">
-                    {{-- @foreach ($languages as $language)
-                    @php
-                        $categories = \App\Models\Category::where('language', $language->lang)->orderByDesc('id')->get();
-                    @endphp
-                    <div class="tab-pane fade show {{ $loop->index === 0 ? 'active' : '' }}" id="home-{{ $language->lang }}" role="tabpanel" aria-labelledby="home-tab2">
-                        <div class="card-body">
-                            <div class="table-responsive">
-                                <table class="table table-striped" id="table-{{ $language->lang }}">
-                                    <thead>
-                                        <tr>
-                                            <th class="text-center">
-                                                #
-                                            </th>
-                                            <th>{{ __('Category Name') }}</th>
-                                            <th>{{ __('Category Code') }}</th>
-                                            <th>{{ __('Displaying at Navigation') }}</th>
-                                            <th>{{ __('Status') }}</th>
-                                            <th>{{ __('Action') }}</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        @foreach ($categories as $category)
-                                        <tr>
-                                            <td>{{ $category->id }}</td>
-                                            <td>{{ $category->name }}</td>
-                                            <td>{{ $category->language }}</td>
-                                            <td>
-                                                @if ($category->show_at_nav == 1)
-                                                    <span class="badge badge-primary">{{ __('Yes') }}</span>
-                                                @else
-                                                     <span class="badge badge-warning">{{ __('No') }}</span>
-                                                @endif
-                                            </td>
-                                            <td>
-                                                @if ($category->status == 1)
-                                                    <span class="badge badge-success">{{ __('Yes') }}</span>
-                                                @else
-                                                     <span class="badge badge-warning">{{ __('No') }}</span>
-                                                @endif
-                                            </td>
-
-                                            <td>
-                                                <a href="{{ route('admin.category.edit', $category->id)}}" class="btn btn-primary"><i class="fas fa-edit"></i></a>
-                                                <a href="{{ route('admin.category.destroy', $category->id) }}" class="btn btn-danger delete-item"><i class="fas fa-trash-alt"></i></a>
-                                            </td>
-                                        </tr>
-                                        @endforeach
-                                    </tbody>
-                                </table>
+                    @foreach ($languages as $language)
+                        @php
+                            $news = \App\Models\News::where('language', $language->lang)
+                                ->orderBy('id', 'DESC')
+                                ->get();
+                        @endphp
+                        <div class="tab-pane fade show {{ $loop->index === 0 ? 'active' : '' }}"
+                            id="home-{{ $language->lang }}" role="tabpanel" aria-labelledby="home-tab2">
+                            <div class="card-body">
+                                <div class="table-responsive">
+                                    <table class="table table-striped" id="table-{{ $language->lang }}">
+                                        <thead>
+                                            <tr>
+                                                <th class="text-center">
+                                                    #
+                                                </th>
+                                                <th>{{ __('Image') }}</th>
+                                                <th>{{ __('Title') }}</th>
+                                                <th>{{ __('Category') }}</th>
+                                                <th>{{ __('In Breaking') }}</th>
+                                                <th>{{ __('In Slider') }}</th>
+                                                <th>{{ __('In Popular') }}</th>
+                                                <th>{{ __('Status') }}</th>
+                                                <th>{{ __('Action') }}</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            @foreach ($news as $item)
+                                                <tr>
+                                                    <td>{{ $item->id }}</td>
+                                                    <td>
+                                                        <img src="{{ asset($item->image) }}" width="100" alt="">
+                                                    </td>
+                                                    <td>{{ $item->title }}</td>
+                                                    <td>{{ $item->category->name }}</td>
+                                                    <td>
+                                                        <label class="custom-switch mt-2">
+                                                            <input {{ $item->is_breaking_news === 1 ? 'checked' : '' }}
+                                                                data-id="{{ $item->id }}" data-name="is_breaking_news"
+                                                                value="1" type="checkbox"
+                                                                class="custom-switch-input toggle-status">
+                                                            <span class="custom-switch-indicator"></span>
+                                                        </label>
+                                                    </td>
+                                                    <td>
+                                                        <label class="custom-switch mt-2">
+                                                            <input {{ $item->show_at_slider === 1 ? 'checked' : '' }}
+                                                                data-id="{{ $item->id }}" data-name="show_at_slider"
+                                                                value="1" type="checkbox"
+                                                                class="custom-switch-input toggle-status">
+                                                            <span class="custom-switch-indicator"></span>
+                                                        </label>
+                                                    </td>
+                                                    <td>
+                                                        <label class="custom-switch mt-2">
+                                                            <input {{ $item->show_at_popular === 1 ? 'checked' : '' }}
+                                                                data-id="{{ $item->id }}" data-name="show_at_popular"
+                                                                value="1" type="checkbox"
+                                                                class="custom-switch-input toggle-status">
+                                                            <span class="custom-switch-indicator"></span>
+                                                        </label>
+                                                    </td>
+                                                    <td>
+                                                        <label class="custom-switch mt-2">
+                                                            <input {{ $item->status === 1 ? 'checked' : '' }}
+                                                                data-id="{{ $item->id }}" data-name="status"
+                                                                value="1" type="checkbox"
+                                                                class="custom-switch-input toggle-status">
+                                                            <span class="custom-switch-indicator"></span>
+                                                        </label>
+                                                    </td>
+                                                    <td>
+                                                        <a href="{{ route('admin.news.edit', $item->id) }}"
+                                                            class="btn btn-primary"><i class="fas fa-edit"></i></a>
+                                                        <a href="{{ route('admin.news.destroy', $item->id) }}"
+                                                            class="btn btn-danger delete-item"><i
+                                                                class="fas fa-trash-alt"></i></a>
+                                                    </td>
+                                                </tr>
+                                            @endforeach
+                                        </tbody>
+                                    </table>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    @endforeach --}}
+                    @endforeach
                 </div>
             </div>
 
@@ -93,7 +127,10 @@
                 "columnDefs": [{
                     "sortable": false,
                     "targets": [2, 3]
-                }]
+                }],
+                "order": [
+                    [0, 'desc']
+                ]
             });
         @endforeach
     </script>

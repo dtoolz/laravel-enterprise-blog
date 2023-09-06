62e67e6883b37c27024edb67d2a9eb022f4ec3da
dtoolz <donaldaudriel@gmail.com>
2023-09-06 22:36:18 +0100

    showing news posts at breaking news section depending on selected language

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -11,10 +11,8 @@ class HomeController extends Controller
     public function index()
     {
         $breakingNews = News::where([
-            'status' => 1,
-            'is_approved' => 1,
             'is_breaking_news' => 1
-        ])->get();
+        ])->GetActiveNews()->GetLocalizedLanguage()->orderBy('id', 'DESC')->take(9)->get();
         return view('frontend.home', compact('breakingNews'));
     }
 }
--- app/Models/News.php
+++ app/Models/News.php
@@ -9,6 +9,22 @@ class News extends Model
 {
     use HasFactory;
 
+    //eloquent model method of scope to get active and approved news
+    public function scopeGetActiveNews($query)
+    {
+         return $query->where([
+            'status' => 1,
+            'is_approved' => 1,
+         ]);
+    }
+    //eloquent model method of scope to get selected language by the user
+    public function scopeGetLocalizedLanguage($query)
+    {
+         return $query->where([
+            'language' => getLanguage()
+         ]);
+    }
+
     public function category()
     {
         return $this->belongsTo(Category::class);

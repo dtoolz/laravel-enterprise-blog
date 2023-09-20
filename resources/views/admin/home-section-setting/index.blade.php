c4059728c3541f8b675cf027cc80edff45a27eeb
dtoolz <donaldaudriel@gmail.com>
2023-09-20 16:44:40 +0100

    updating and retrieving the choice of categories arrangements (homeSectionSettings) made by the admin at the admin panel

--- resources/views/admin/home-section-setting/index.blade.php
+++ resources/views/admin/home-section-setting/index.blade.php
@@ -19,21 +19,82 @@
                 <ul class="nav nav-tabs" id="myTab2" role="tablist">
                     @foreach ($languages as $language)
                         <li class="nav-item">
-                            <a class="nav-link {{ $loop->index === 0 ? 'active' : '' }}" id="home-tab2" data-toggle="tab" href="#home-{{ $language->lang }}" role="tab"
-                                aria-controls="home" aria-selected="true">{{ $language->name }}</a>
+                            <a class="nav-link {{ $loop->index === 0 ? 'active' : '' }}" id="home-tab2" data-toggle="tab"
+                                href="#home-{{ $language->lang }}" role="tab" aria-controls="home"
+                                aria-selected="true">{{ $language->name }}</a>
                         </li>
                     @endforeach
                 </ul>
                 <div class="tab-content tab-bordered" id="myTab3Content">
                     @foreach ($languages as $language)
-                    @php
-                        $categories = \App\Models\Category::where('language', $language->lang)->orderByDesc('id')->get();
-                    @endphp
-                    <div class="tab-pane fade show {{ $loop->index === 0 ? 'active' : '' }}" id="home-{{ $language->lang }}" role="tabpanel" aria-labelledby="home-tab2">
-                        <div class="card-body">
+                        @php
+                            $categories = \App\Models\Category::where('language', $language->lang)
+                                ->orderByDesc('id')
+                                ->get();
+                            $homeSectionSetting = \App\Models\HomeSectionSetting::where('language', $language->lang)->first();
+                        @endphp
+                        <div class="tab-pane fade show {{ $loop->index === 0 ? 'active' : '' }}"
+                            id="home-{{ $language->lang }}" role="tabpanel" aria-labelledby="home-tab2">
+                            <div class="card-body">
+                                <form action="{{ route('admin.home-section-setting.update') }}" method="POST">
+                                    @csrf
+                                    @method('PUT')
+                                    <div class="form-group">
+                                        <label for="">{{ __('Category Section one') }}</label>
+                                        <input type="hidden" name="language" value="{{ $language->lang }}">
+                                        <select name="category_section_one" id="" class="form-control select2">
+                                            <option value="">---{{ __('Select') }}---</option>
+                                            @foreach ($categories as $category)
+                                                <option
+                                                    {{ @$homeSectionSetting->category_section_one == $category->id ? 'selected' : '' }}
+                                                    value="{{ $category->id }}">{{ $category->name }}</option>
+                                            @endforeach
+                                        </select>
 
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="">{{ __('Category Section Two') }}</label>
+                                        <select name="category_section_two" id="" class="form-control select2">
+                                            <option value="">---{{ __('Select') }}---</option>
+                                            @foreach ($categories as $category)
+                                                <option
+                                                    {{ @$homeSectionSetting->category_section_two == $category->id ? 'selected' : '' }}
+                                                    value="{{ $category->id }}">{{ $category->name }}</option>
+                                            @endforeach
+                                        </select>
+
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="">{{ __('Category Section Three') }}</label>
+                                        <select name="category_section_three" id="" class="form-control select2">
+                                            <option value="">---{{ __('Select') }}---</option>
+                                            @foreach ($categories as $category)
+                                                <option
+                                                    {{ @$homeSectionSetting->category_section_three == $category->id ? 'selected' : '' }}
+                                                    value="{{ $category->id }}">{{ $category->name }}</option>
+                                            @endforeach
+                                        </select>
+
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="">{{ __('Category Section Four') }}</label>
+                                        <select name="category_section_four" id="" class="form-control select2">
+                                            <option value="">---{{ __('Select') }}---</option>
+                                            @foreach ($categories as $category)
+                                                <option
+                                                    {{ @$homeSectionSetting->category_section_four == $category->id ? 'selected' : '' }}
+                                                    value="{{ $category->id }}">{{ $category->name }}</option>
+                                            @endforeach
+                                        </select>
+
+                                    </div>
+                                    <button type="submit" class="btn btn-primary">{{ __('Save') }}</button>
+                                </form>
+                            </div>
                         </div>
-                    </div>
                     @endforeach
                 </div>
             </div>
@@ -44,6 +105,13 @@
 
 @push('scripts')
     <script>
-
+        @if ($errors->any())
+            @foreach ($errors->all() as $error)
+                Toast.fire({
+                    icon: 'error',
+                    title: "{{ $error }}"
+                });
+            @endforeach
+        @endif
     </script>
 @endpush

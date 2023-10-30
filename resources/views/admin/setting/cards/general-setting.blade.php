dc7a75df2eda34236e74eee920c2d35d6ca8b566
dtoolz <donaldaudriel@gmail.com>
2023-10-30 14:05:13 +0100

    showing admin updated color across the website dynamically

--- resources/views/admin/setting/cards/general-setting.blade.php
+++ resources/views/admin/setting/cards/general-setting.blade.php
@@ -7,16 +7,25 @@
                 <label for="">{{ __('Website Name') }}</label>
                 <input type="text" name="site_name" class="form-control" value="{{ $settings['site_name'] }}">
             </div>
+            @error('site_name')
+                <p class="text-danger">{{ $message }}</p>
+            @enderror
             <div class="form-group">
                 <img src="{{ asset($settings['site_logo']) }}" alt="" width="150"><br>
                 <label for="">{{ __('Website Logo') }}</label>
                 <input type="file" name="site_logo" class="form-control">
             </div>
+            @error('site_logo')
+                <p class="text-danger">{{ $message }}</p>
+            @enderror
             <div class="form-group">
                 <img src="{{ asset($settings['site_favicon']) }}" alt="" width="150"><br>
                 <label for="">{{ __('Website Favicon') }}</label>
                 <input type="file" name="site_favicon" class="form-control">
             </div>
+            @error('site_favicon')
+                <p class="text-danger">{{ $message }}</p>
+            @enderror
             <button type="submit" class="btn btn-primary">{{ __('Save') }}</button>
         </form>
     </div>
--- resources/views/admin/setting/cards/seo-setting.blade.php
+++ resources/views/admin/setting/cards/seo-setting.blade.php
@@ -7,14 +7,23 @@
                 <label for="">{{ __('Website Seo Title') }}</label>
                 <input type="text" name="site_seo_title" class="form-control" value="{{ $settings['site_seo_title'] }}">
             </div>
+            @error('site_seo_title')
+                    <p class="text-danger">{{ $message }}</p>
+                    @enderror
             <div class="form-group">
                 <label for="">{{ __('Website Seo Description') }}</label>
                 <textarea name="site_seo_description" class="form-control" style="height: 300px" cols="30" rows="10">{{ $settings['site_seo_description'] }}</textarea>
             </div>
+            @error('site_seo_description')
+            <p class="text-danger">{{ $message }}</p>
+            @enderror
             <div class="form-group">
                 <label for="">{{ __('Website Seo Keywords') }}</label>
                 <input name="site_seo_keywords" type="text" class="form-control inputtags" value="{{ $settings['site_seo_keywords'] }}">
             </div>
+            @error('site_seo_keywords')
+            <p class="text-danger">{{ $message }}</p>
+            @enderror
             <button type="submit" class="btn btn-primary">{{ __('Save') }}</button>
         </form>
     </div>

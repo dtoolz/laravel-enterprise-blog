ef94545d661bc980d03199708542559ec1ffcc94
dtoolz <donaldaudriel@gmail.com>
2023-11-06 15:22:30 +0100

    added auto approval on creating news posts for super admin or news all access permission

--- app/Helpers/helper.php
+++ app/Helpers/helper.php
@@ -84,3 +84,18 @@ function canAccess(array $permissions)
     }
 
 }
+
+// get admin role
+
+function getRole()
+{
+    $role = auth()->guard('admin')->user()->getRoleNames();
+    return $role->first();
+}
+
+// check user permission
+
+function checkPermission(string $permission)
+{
+    return auth()->guard('admin')->user()->hasPermissionTo($permission);
+}
--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -89,6 +89,7 @@ class NewsController extends Controller
         $news->show_at_slider = $request->show_at_slider == 1 ? 1 : 0;
         $news->show_at_popular = $request->show_at_popular == 1 ? 1 : 0;
         $news->status = $request->status == 1 ? 1 : 0;
+        $news->is_approved = getRole() == 'Super Admin' || checkPermission('news all-access') ? 1 : 0;
         $news->save();
 
         $tags = explode(',', $request->tags);
